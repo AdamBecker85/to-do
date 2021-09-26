f6ec2a304ef21b7886bb94339693f26b71aa43f0
Adam Becker <amkbecker@gmail.com>
2021-09-26 10:40:22 +0200

    Switch img source to App component and add Column component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import styles from './List.scss';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column'
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    image: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} image={this.props.image}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column columnTitle={'Animals'}/>
+          <Column columnTitle={'Plants'}/>
+          <Column columnTitle={'Minerals'}/>
+        </div>
       </section>
     )
   }
