31ea841bbc0003bbd77a5fe9f973b34f87a642c9
Adam Becker <amkbecker@gmail.com>
2021-10-19 16:11:06 +0200

    Add SearchResults and ListLink components

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -92,6 +92,18 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Take care of my car',
+    description: 'Important things I want to do!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+  {
+    id: 'list-3',
+    title: 'Clean my flat',
+    description: 'Unnecessary things I have to do!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
 ];
 
 const columns = [
